28aabe0496a6ca15d431f208226bac52b3f7e513
Sujanshrestha <sujanstha5732@gmail.com>
2024-12-13 09:44:25 +0545

    1st django class

--- book_review/views.py
+++ book_review/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+
+
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, 'index.html')
 
 def contact(request):
     return HttpResponse("<h1> this is contact route </h1>")  
 
 def about(request):
     return HttpResponse("this is about route")     
+
